81bdb7a15dd55883e451b10645fa31c16b60cd71
hugohiga-ufabc <32755058+hugohiga-ufabc@users.noreply.github.com>
2018-06-06 00:04:58 -0300

    c1
    c1

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@ var url = "/";
 var text = " { replies: [{ type: 'text', content: 'Roger that' }], conversation: { memory: { key: 'value' }}}";
 
 $.ajax({
-    data: JSON.stringify(text),
+    data: $.response.setBody(JSON.stringify(text)),
     dataType: 'json',
     processData: false,
     type: 'POST',
